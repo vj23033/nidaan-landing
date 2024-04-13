335712768cfbd0990c4d222b2269273f2e2bdd9e
vj23033 <162152714+vj23033@users.noreply.github.com>
2024-04-13 15:03:50 +0530

    Update Navbar.jsx

--- nidaan-landing/src/app/components/UI/Navbar.jsx
+++ nidaan-landing/src/app/components/UI/Navbar.jsx
@@ -29,6 +29,7 @@ const Navbar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
+             <img className="h-10 w-10 mr-2" src='/logo.jpeg' alt="Logo" />
             {/* <img className="h-10 w-10 mr-2" src={logo} alt="Logo" /> */}
             <span className="text-xl font-bold tracking-tight">Logoipsum</span>
           </div>
